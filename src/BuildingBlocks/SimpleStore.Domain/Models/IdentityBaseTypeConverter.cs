530ea509febeaf199f3627d21f09610cc22788ce
Kim CH <kim.cuhoang@gmail.com>
2020-03-05 16:42:36 +0700

    Feature/format json strongly typeid (#4)
    * Define custom of TypeConverter and JsonConverter
    
    * Add UpdateProduct usecase
    
    * Update README

---
+++ src/BuildingBlocks/SimpleStore.Domain/Models/IdentityBaseTypeConverter.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.ComponentModel;
+using System.Globalization;
+
+namespace SimpleStore.Domain.Models
+{
+    public class IdentityBaseTypeConverter<TIdentity> : TypeConverter where TIdentity : IdentityBase
+    {
+        #region Overrides of TypeConverter
+
+        public override bool CanConvertFrom(ITypeDescriptorContext context, Type sourceType)
+        {
+            return sourceType == typeof(string) || base.CanConvertFrom(context, sourceType);
+        }
+
+        public override object ConvertFrom(ITypeDescriptorContext context, CultureInfo culture, object value)
+        {
+            var stringValue = value as string;
+            if (!string.IsNullOrEmpty(stringValue) && Guid.TryParse(stringValue, out var guid))
+            {
+                return IdentityFactory.Create<TIdentity>(guid);
+            }
+
+            return base.ConvertFrom(context, culture, value);
+        }
+
+        #endregion
+    }
+}
---
+++ src/Services/ProductCatalog/SimpleStore.ProductCatalog.Infrastructure.EfCore/UseCases/UpdateProduct/UpdateProductRequestValidator.cs
@@ -0,0 +1,18 @@
+﻿using FluentValidation;
+
+namespace SimpleStore.ProductCatalog.Infrastructure.EfCore.UseCases.UpdateProduct
+{
+    public class UpdateProductRequestValidator : AbstractValidator<UpdateProductRequest>
+    {
+        public UpdateProductRequestValidator()
+        {
+            RuleFor(x => x.ProductId)
+                .Cascade(CascadeMode.StopOnFirstFailure)
+                .NotNull();
+
+            RuleFor(x => x.NewProductName)
+                .Cascade(CascadeMode.StopOnFirstFailure)
+                .NotNull().NotEmpty();
+        }
+    }
+}
