83f07a574e9e61beb4b66f5e02401deefe8504d0
kimcu-on-thenet <kim.cuhoang@gmail.com>
2020-03-05 14:02:52 +0700

    Refactore code - separating extensions for ServiceCollectionExtension: AddCustomValidators, AddCustomerMediatR ...

--- src/Services/ProductCatalog/SimpleStore.ProductCatalogApi/Startup.cs
+++ src/Services/ProductCatalog/SimpleStore.ProductCatalogApi/Startup.cs
@@ -1,14 +1,8 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 using SimpleStore.ProductCatalog.Infrastructure.EfCore;
 
 namespace SimpleStore.ProductCatalogApi
@@ -22,15 +16,19 @@ namespace SimpleStore.ProductCatalogApi
 
         public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+            
             services.AddSingleton(this.Configuration);
-            services.AddEfCore();
+
+            services
+                .AddEfCore()
+                .AddCustomMediatR()
+                .AddCustomValidators()
+                .AddCustomHostedServices();
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
