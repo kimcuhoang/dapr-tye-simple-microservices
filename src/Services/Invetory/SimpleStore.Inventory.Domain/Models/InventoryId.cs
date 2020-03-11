732c194b0f907581177d484b273945667c71684e
kimcu-on-thenet <kim.cuhoang@gmail.com>
2020-03-11 16:57:41 +0700

    Define SimpleStore.Inventory.Domain - InventoryId & Inventory

---
+++ src/Services/Invetory/SimpleStore.Inventory.Domain/Models/InventoryId.cs
@@ -0,0 +1,12 @@
+﻿using SimpleStore.Domain.Models;
+using System;
+
+namespace SimpleStore.Inventory.Domain.Models
+{
+    public class InventoryId : IdentityBase
+    {
+        private InventoryId(Guid id) : base(id) { }
+
+        public static explicit operator InventoryId(Guid id) => id == Guid.Empty ? null : new InventoryId(id);
+    }
+}
