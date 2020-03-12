7ece9bff0fa133d7fb3d10a42962e5912b0992b9
kimcu-on-thenet <kim.cuhoang@gmail.com>
2020-03-12 16:15:01 +0700

    Integrate with GraphQLApi

--- src/Services/GraphQL/SimpleStore.GraphQLApi/Options/ServiceOptions.cs
+++ src/Services/GraphQL/SimpleStore.GraphQLApi/Options/ServiceOptions.cs
@@ -6,5 +6,6 @@ namespace SimpleStore.GraphQLApi.Options
     {
         public ServiceConfig GraphQLApi { get; set; }
         public ServiceConfig ProductCatalogApi { get; set; }
+        public ServiceConfig InventoryApi { get; set; }
     }
 }
