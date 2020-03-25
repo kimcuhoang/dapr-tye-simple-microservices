1ce52fe83ee9809dbcca3473156703fee9297779
kimcu-on-thenet <kim.cuhoang@gmail.com>
2020-03-25 17:21:28 +0700

    support docker (not finished yet_ - connectionstring does not update through docker environment

--- src/BuildingBlocks/SimpleStore.Infrastructure.EfCore/SqlServer/SqlServerDbContextOptionsBuilder.cs
+++ src/BuildingBlocks/SimpleStore.Infrastructure.EfCore/SqlServer/SqlServerDbContextOptionsBuilder.cs
@@ -1,22 +1,27 @@
 ﻿using System.Reflection;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
 using SimpleStore.Infrastructure.EfCore.Persistence;
 
 namespace SimpleStore.Infrastructure.EfCore.SqlServer
 {
     public class SqlServerDbContextOptionsBuilder : IExtendDbContextOptionsBuilder
     {
+        private readonly ILogger<SqlServerDbContextOptionsBuilder> _logger;
         private readonly IConnectionStringFactory _connectionStringFactory;
 
-        public SqlServerDbContextOptionsBuilder(IConnectionStringFactory connectionStringFactory)
+        public SqlServerDbContextOptionsBuilder(IConnectionStringFactory connectionStringFactory, ILogger<SqlServerDbContextOptionsBuilder> logger)
         {
             this._connectionStringFactory = connectionStringFactory;
+            this._logger = logger;
         }
 
         #region Implementation of IExtendDbContextOptionsBuilder
 
         public DbContextOptionsBuilder Extend(DbContextOptionsBuilder optionsBuilder, IConnectionStringFactory connectionStringFactory, string assemblyName = null)
         {
+            var connectionString = this._connectionStringFactory.Create();
+            this._logger.LogInformation(connectionString);
             return optionsBuilder.UseSqlServer(this._connectionStringFactory.Create(), opts =>
                 {
                     opts.MigrationsAssembly(assemblyName);
