b7d10ca9773d721768de5e3cb86e730c2d13b4a3
kimcu-on-thenet <kim.cuhoang@gmail.com>
2020-06-13 10:18:38 +0700

    Remove redundant codes

--- src/Services/ProductCatalog/SimpleStore.ProductCatalog.Infrastructure.EfCore/Gateways/DaprInventoriesGateway.cs
+++ src/Services/ProductCatalog/SimpleStore.ProductCatalog.Infrastructure.EfCore/Gateways/DaprInventoriesGateway.cs
@@ -1,11 +1,9 @@
 ﻿using Dapr.Client;
 using Dapr.Client.Http;
 using Microsoft.Extensions.Options;
+using SimpleStore.ProductCatalog.Infrastructure.EfCore.Gateways.UseCases.GetProductsByIds;
 using SimpleStore.ProductCatalog.Infrastructure.EfCore.Options;
 using System.Threading.Tasks;
-using SimpleStore.ProductCatalog.Infrastructure.EfCore.Gateways.UseCases.GetInventories;
-using SimpleStore.ProductCatalog.Infrastructure.EfCore.Gateways.UseCases.GetProducts;
-using SimpleStore.ProductCatalog.Infrastructure.EfCore.Gateways.UseCases.GetProductsByIds;
 
 namespace SimpleStore.ProductCatalog.Infrastructure.EfCore.Gateways
 {
@@ -26,12 +24,6 @@ namespace SimpleStore.ProductCatalog.Infrastructure.EfCore.Gateways
             this._daprClient = daprClient;
             this._serviceOptions = serviceOptions.Value;
         } 
-
-        public async Task<GetInventoriesResponse> GetInventories(GetInventoriesRequest request)
-        => await this._daprClient.InvokeMethodAsync<GetInventoriesRequest, GetInventoriesResponse>(this.InventoryAppId, "get-inventories", request, this.httpExtension);
-
-        public async Task<GetProductsResponse> GetProducts(GetProductsRequest request)
-        => await this._daprClient.InvokeMethodAsync<GetProductsRequest, GetProductsResponse>(this.InventoryAppId, "get-products", request, this.httpExtension);
 
         public async Task<GetProductsByIdsResponse> GetProductsByIds(GetProductsByIdsRequest request)
         => await this._daprClient.InvokeMethodAsync<GetProductsByIdsRequest, GetProductsByIdsResponse>(this.InventoryAppId, "get-products-by-ids", request, this.httpExtension);
--- src/Services/ProductCatalog/SimpleStore.ProductCatalog.Infrastructure.EfCore/Gateways/UseCases/GetProducts/GetProductsRequest.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace SimpleStore.ProductCatalog.Infrastructure.EfCore.Gateways.UseCases.GetProducts
-{
-    public class GetProductsRequest
-    {
-        public int PageIndex { get; set; } = 1;
-        public int PageSize { get; set; } = 10;
-    }
-}
